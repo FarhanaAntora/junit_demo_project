951b4f284a2db0b8eb6e2fd3009ea365ab334feb
FarhanaAntora <jahanfarhana997@gmail.com>
2022-01-22 19:40:28 +0600

    added new method

--- src/test/java/MyJunit.java
+++ src/test/java/MyJunit.java
@@ -1,22 +1,26 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 import static java.lang.Thread.sleep;
 
 public class MyJunit {
@@ -115,53 +119,197 @@ public class MyJunit {
     }
 
     @Test
-    public void selectDropdown() {
+    public void selectDropdown() throws InterruptedException {
         driver.get("https://demoqa.com/select-menu");
         Select color = new Select(driver.findElement(By.id("oldSelectMenu")));
-        color.selectByValue("3");
+        color.selectByValue("2");
         Select cars = new Select(driver.findElement(By.id("cars")));
-        if (cars.isMultiple()) {
+        /*if (cars.isMultiple()) {
             cars.selectByValue("volvo");
             cars.selectByValue("audi");
-        }
+        }*/
     }
+
     @Test
     public void handleNewTab() throws InterruptedException {
         driver.get("https://demoqa.com/links");
         driver.findElement(By.id("simpleLink")).click();
-
-
         ArrayList<String> w = new ArrayList<String>(driver.getWindowHandles());
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
 
         //switch to open tab
         driver.switchTo().window(w.get(1));
         String title = driver.getTitle();
         System.out.println("New tab title: " + driver.getTitle());
-
-//        for(String childTab:driver.getWindowHandles()){
-//            driver.switchTo().window(childTab);
-//        }
+        //for(String childTab:driver.getWindowHandles()){
+        //driver.switchTo().window(childTab);
+        //}
         //System.out.println(driver.getTitle());
-        //wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        //WebElement imgElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='/images/Toolsqa.jpg']")));
-        //boolean status = imgElement.isDisplayed();
-        Assert.assertTrue(title.contains("ToolsQA"));
-        Boolean status = driver.findElement(By.xpath("//img[@src='/images/Toolsqa.jpg']")).isDisplayed();
-        Assert.assertEquals(true,status);
-        //driver.close();
+        //Assert.assertTrue(title.contains("ToolsQA"));
+        //Boolean status = driver.findElement(By.xpath("//img[@src='/images/Toolsqa.jpg']")).isDisplayed();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(80));
+        WebElement imgElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='/images/Toolsqa.jpg']")));
+        boolean status = imgElement.isDisplayed();
+        Assert.assertEquals(true, status);
+        driver.close();
         driver.switchTo().window(w.get(0));
     }
 
+    @Test
+    public void handleChildWindow() {
+
+        driver.get("https://demoqa.com/browser-windows");
+        //Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("windowButton")));
+        driver.findElement(By.id(("windowButton"))).click();
+        //WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("windowButton")));
+        //button.click();
+        String mainWindowHandle = driver.getWindowHandle();
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        Iterator<String> iterator = allWindowHandles.iterator();
+
+        while (iterator.hasNext()) {
+            String ChildWindow = iterator.next();
+            if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
+                driver.switchTo().window(ChildWindow);
+                String text = driver.findElement(By.id("sampleHeading")).getText();
+                Assert.assertTrue(text.contains("This is a sample page"));
+                //driver.close();
+            }
+
+        }
+    }
+
+    @Test
+    public void modalDialog() {
+
+        driver.get("https://demoqa.com/modal-dialogs");
+        //WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("showSmallModal")));
+        //element.click();
+        driver.findElement(By.id("showSmallModal")).click();
+        driver.findElement(By.id("closeSmallModal")).click();
+
+    }
+
+    @Test
+    public void webTables() {
+        driver.get("https://demoqa.com/webtables");
+        driver.findElement(By.xpath("//span[@id='edit-record-1']//*[@stroke='currentColor']")).click();
+        driver.findElement(By.id("submit")).click();
+
+    }
+
+    @Test
+    public void scrapData() {
+        driver.get("https://demoqa.com/webtables");
+        WebElement table = driver.findElement(By.className("rt-tbody"));
+        List<WebElement> allRows = table.findElements(By.className("rt-tr"));
+        int i = 0;
+        for (WebElement row : allRows) {
+            List<WebElement> cells = row.findElements(By.className("rt-td"));
+            for (WebElement cell : cells) {
+                i++;
+                System.out.println("num[" + i + "] " + cell.getText());
+
+            }
+        }
+    }
+
+    @Test
+    public void uploadImage() {
+        driver.get("https://demoqa.com/upload-download");
+        WebElement uploadElement = driver.findElement(By.id("uploadFile"));
+        uploadElement.sendKeys("D:\\A.jpg");
+
+        String text = driver.findElement(By.id("uploadedFilePath")).getText();
+        Assert.assertTrue(text.contains("A.jpg"));
+    }
+
+    @Test
+    public void handleIframe() {
+        driver.get("https://demoqa.com/frames");
+        driver.switchTo().frame("frame1");
+        String text = driver.findElement(By.id("sampleHeading")).getText();
+        Assert.assertTrue(text.contains("This is a sample page"));
+        driver.switchTo().defaultContent();
+
+    }
+
+    @Test
+    public void mouseHover() throws InterruptedException {
+        driver.get("https://green.edu.bd/");
+        WebElement menuAboutElement = driver.findElement(By.xpath("//a[@class='dropdown-toggle'][contains(text(),'About Us')]"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(menuAboutElement).perform();
+        //List <WebElement> menuAboutElement = driver.findElements(By.xpath("//a[contains(text(),'About Us')]"));
+        //Actions actions = new Actions(driver);
+        //actions.moveToElement(menuAboutElement.get(2)).perform();
+        Thread.sleep(3000);
+        WebElement subMenu = driver.findElement(By.xpath("//li[@id='menu-item-325']//a[contains(text(),'History')]"));
+        actions.moveToElement(subMenu);
+        actions.click().build().perform();
+    }
+
+    @Test
+    public void keyboardEvents() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        WebElement searchElement = driver.findElement(By.name("q"));
+        Actions action = new Actions(driver);
+        action.moveToElement(searchElement);
+        action.keyDown(Keys.SHIFT);
+        action.sendKeys("Selenium Webdriver")
+                .keyUp(Keys.SHIFT)
+                .doubleClick()
+                .contextClick()
+                .perform();
+        Thread.sleep(5000);
+    }
 
 
-      @After
-        public void closeBrowser(){
-            //driver.close();
-            driver.quit();
+    @Test
+    public void takeScreenShot() throws IOException {
+        driver.get("https://demoqa.com");
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new Date());
+        String fileWithPath = "./src/test/resources/screenshots/" + time + ".png";
+        File DestFile = new File(fileWithPath);
+        FileUtils.copyFile(screenshotFile, DestFile);
+    }
 
 
+    public static void readFromExcel(String filePath, String fileName, String sheetName) throws IOException {
+        File file = new File(filePath + "\\" + fileName);
+        FileInputStream inputStream = new FileInputStream(file);
+        Workbook workbook = null;
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+        if (fileExtensionName.equals(".xls")) {
+             workbook = new HSSFWorkbook(inputStream);
+        }
+        Sheet sheet = workbook.getSheet(sheetName);
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+        for (int i = 0; i < rowCount + 1; i++) {
+            Row row = sheet.getRow(i);
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                //System.out.print((row.getCell(j).getStringCellValue()) + "|| ");
+                DataFormatter formatter = new DataFormatter();
+                System.out.print(formatter.formatCellValue((row.getCell(j)))+"|| ");
+            }
+            System.out.println();
 
+        }
+    }
+
+    @Test
+    public void readExcelFile() throws IOException {
+        readFromExcel("D:\\", "Book1.xls", "sheet1");
+
+    }
+
+    @After
+    public void closeBrowser() {
+        //driver.close();
+        //driver.quit();
 
     }
 
